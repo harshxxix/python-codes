8533e46b9cf74d20009b13283bf6ce0402a8d2ac
harshxxix <darandaleharshal3933@gmail.com>
2025-02-07 12:36:18 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,5 @@
+pi = 3.14
+r = int(input("Radius of circle is: "))
+
+area = pi*r*r
+print("Area of circle of given radius is: ",area)
---
+++ percentage.py
@@ -0,0 +1,46 @@
+m1 = int(input("Marks of FPL: "))
+m2 = int(input("Marks of Physics: "))
+m3 = int(input("Marks of Mechanics: "))
+m4 = int(input("Marks of M1: "))
+m5 = int(input("Marks of BXE: "))
+
+total_marks = m1+m2+m3+m4+m5
+print("Total marks: ",total_marks)
+
+perc = (total_marks/500)*100
+print("Percentage: ",perc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
